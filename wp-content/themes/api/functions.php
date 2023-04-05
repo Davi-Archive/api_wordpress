79ff0a7116e18ef5ea2d95685101a036d877beb8
d1av <davi4alves@gmail.com>
2023-04-05 20:50:54 -0300

    :tada: Initial commit

---
+++ wp-content/themes/api/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+$template_directory = get_template_directory();
+
+ require_once($template_directory . "/custom-post-type/product.php");
+ require_once($template_directory . "/custom-post-type/transacao.php");
+ 
+ require_once($template_directory . "/endpoints/usuario_post.php");
+
+?>
