6af57a82c4a13a3c7b86c55a85766e3c08029eba
d1av <davi4alves@gmail.com>
2023-04-05 22:15:58 -0300

    feat: :sparkles: add jwt token

---
+++ wp-content/themes/api/endpoints/usuario_get.php
@@ -0,0 +1,35 @@
+<?php
+
+function api_usuario_get($request)
+{
+
+    $user = wp_get_current_user();
+    $user_id = $user->ID;
+
+    if($user_id>0) {
+        $user_meta = get_user_meta($user_id);
+
+        $response = array
+
+
+
+    }
+
+    return rest_ensure_response($user);
+}
+
+function registrar_api_usuario_get()
+{
+    register_rest_route(
+        'api',
+        '/usuario',
+        array(
+        array(
+          'methods' => WP_REST_Server::READABLE,
+          'callback' => 'api_usuario_get',
+        ),
+    )
+    );
+}
+
+add_action('rest_api_init', 'registrar_api_usuario_get');
--- wp-content/themes/api/functions.php
+++ wp-content/themes/api/functions.php
@@ -2,9 +2,16 @@
 
 $template_directory = get_template_directory();
 
- require_once($template_directory . "/custom-post-type/product.php");
- require_once($template_directory . "/custom-post-type/transacao.php");
- 
- require_once($template_directory . "/endpoints/usuario_post.php");
+require_once($template_directory . "/custom-post-type/product.php");
+require_once($template_directory . "/custom-post-type/transacao.php");
 
-?>
+require_once($template_directory . "/endpoints/usuario_post.php");
+require_once($template_directory . "/endpoints/usuario_get.php");
+
+
+function expire_token()
+{
+    return time()+(60*60*24);
+}
+
+add_action('jwt_auth_expire', 'expire_token');
