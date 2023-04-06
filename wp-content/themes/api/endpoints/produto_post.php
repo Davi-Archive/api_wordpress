e71b10cddd4c491928becb5f442268f40cef063a
d1av <davi4alves@gmail.com>
2023-04-06 01:32:24 -0300

    feat: :sparkles: save and get image product

--- wp-content/themes/api/endpoints/produto_post.php
+++ wp-content/themes/api/endpoints/produto_post.php
@@ -32,12 +32,12 @@ function api_produto_post($request)
         $files = $request-> get_file_params();
 
         if($files) {
-            require_once(ABSPATH.'wp-admin/includes/image.php');
-            require_once(ABSPATH.'wp-admin/includes/file.php');
-            require_once(ABSPATH.'wp-admin/includes/media.php');
+            require_once(ABSPATH . 'wp-admin/includes/image.php');
+            require_once(ABSPATH . 'wp-admin/includes/file.php');
+            require_once(ABSPATH . 'wp-admin/includes/media.php');
 
             foreach ($files as $file => $array) {
-                media_handle_upload($file_id, $produto_id);
+                media_handle_upload($file, $produto_id);
             }
         }
 
