ad7a24a677f8e75ee43902e143599f0f3b721d13
d1av <davi4alves@gmail.com>
2023-04-06 00:45:38 -0300

    feat: :sparkles: add product endpoint

--- wp-content/themes/api/functions.php
+++ wp-content/themes/api/functions.php
@@ -7,6 +7,9 @@ require_once($template_directory . "/custom-post-type/transacao.php");
 
 require_once($template_directory . "/endpoints/usuario_post.php");
 require_once($template_directory . "/endpoints/usuario_get.php");
+require_once($template_directory . "/endpoints/usuario_put.php");
+
+require_once($template_directory . "/endpoints/produto_post.php");
 
 
 function expire_token()
